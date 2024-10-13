2f2550f82600bf6ab4bbe61567401280bbf14185
Ace <haroldace12345@gmail.com>
2024-10-12 23:20:23 -0700

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,60 @@
+import javax.swing.*;
+import java.util.StringTokenizer;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String input;
+
+        input = JOptionPane.showInputDialog("Enter number of students: ");
+        int studentAmount = Integer.parseInt(input);
+
+        Student[] st = new Student[studentAmount];
+
+        for (int i = 0; i < st.length; i++) {
+            input = JOptionPane.showInputDialog("Enter student data separated by commas.");
+
+            String delim = ",";
+            String token;
+
+            StringTokenizer stk = new StringTokenizer(input, delim);
+
+            token = stk.nextToken().trim();
+            int id = Integer.parseInt(token);
+
+            String name = stk.nextToken().trim();
+
+            token = stk.nextToken().trim();
+            int studentExams = Integer.parseInt(token);
+            double[] scores = new double[studentExams];
+
+            for (int j = 0; j < studentExams; j++) {
+                token = stk.nextToken().trim();
+                scores[j] = Double.parseDouble(token);
+            }
+
+            st[i] = new Student(id, name, scores);
+        }
+
+        String outA = "", outB = "", outC = "", outD = "", outF = "";
+
+        for (int k = 0; k < st.length; k++) {
+            String grade = st[k].findGrade();
+
+            if (grade.equalsIgnoreCase("A")) {
+                outA += st[k].getId() + " " + st[k].getName() + " (" + grade + ")\n";
+            } else if (grade.equalsIgnoreCase("B")) {
+                outB += st[k].getId() + " " + st[k].getName() + " (" + grade + ")\n";
+            } else if (grade.equalsIgnoreCase("C")) {
+                outC += st[k].getId() + " " + st[k].getName() + " (" + grade + ")\n";
+            } else if (grade.equalsIgnoreCase("D")) {
+                outD += st[k].getId() + " " + st[k].getName() + " (" + grade + ")\n";
+            } else {
+                outF += st[k].getId() + " " + st[k].getName() + " (" + grade + ")\n";
+            }
+        }
+
+        String outAll = outA + outB + outC + outD + outF;
+
+        JOptionPane.showMessageDialog(null, outAll);
     }
 }
